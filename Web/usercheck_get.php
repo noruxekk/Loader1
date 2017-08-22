0886170239e321716eb30c2df812a4ca5879c20e
Tyson <thaisenpm2@gmail.com>
2017-08-21 19:02:37 -0600

    Update usercheck_get.php
    Redone

--- Web/usercheck_get.php
+++ Web/usercheck_get.php
@@ -1,60 +1,37 @@
 <?php
-$link = mysqli_connect('localhost','database_username','database_password');
-$database = mysqli_select_db($link,'database_name');
+$link = mysqli_connect('localhost','root','');
+$database = mysqli_select_db($link,'mybb');
+
+// Create connection
+/*$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} */
 
 $user = $_GET['username'];
 
-$sql = "SELECT * FROM mybb_users WHERE username = '". mysqli_real_escape_string($link,$user) ."'" ;
+$sql = "SELECT * FROM forums_users WHERE username = '". mysqli_real_escape_string($link,$user) ."'" ;
+//$sql = "SELECT * FROM forums_users";
 $result = $link->query($sql);
 
 if ($result->num_rows > 0) {
 	// Outputting the rows
 	while($row = $result->fetch_assoc()) {
 		
-		if (strpos($row['usergroup'], '7') !== FALSE)
-		{
-			echo "User is banned<br>";
-		}
-		else
-		{
-			if (strpos($row['usergroup'], '8') !== FALSE)
-			{
-				echo "User is premium<br>";
-			}
-			else if (strpos($row['additionalgroups'], '8') !== FALSE)
-			{
-				echo "User is premium<br>";
-			}
-			else
-			{
-				echo "User is not premium<br>";
-			}
-		}
-		
-		if (strpos($row['additionalgroups'], '7') !== FALSE)
-		{
-			echo "User is banned<br>";
-		}
-		else
-		{
-
-		}
-		/*echo "Username: " . $row['username'] . "<br>";
-		echo "Primary Group: " . $row['usergroup'] . "<br>";
-		echo "Additonal Groups: " . $row['additionalgroups'] . "<br>";
-		echo "<br>";*/
-		
-		// Usergroup check
-		
-		
-		
+		echo $row['usergroup'];
+		echo $row['additionalgroups'];
+				
 	}
 } 
 else
 {
 	echo "No user with the username " . $user;
 }
-//2: Registered <br> 3: Super Moderator <br> 4: Administrators <br> 5: Awaiting Activation <br> 6: Moderator <br> 7: Banned <br> 8: Premium
+/*
+
+*/
 ?>
 
 <html>
@@ -65,18 +42,4 @@ else
 
 <body>
 <!-- <br><br><a href="/usercheck.php">Return</a> -->
-
-<!---------------------------------------------------
-' Coded by /id/Thaisen! Free loader source
-' https://github.com/ThaisenPM/Cheat-Loader-CSGO
-' Note to the person using this, removing this
-' text is in violation of the license you agreed
-' to by downloading. Only you can see this so what
-' does it matter anyways.
-' Copyright © ThaisenPM 2017
-' Licensed under a MIT license
-' Read the terms of the license here
-' https://github.com/ThaisenPM/Cheat-Loader-CSGO/blob/master/LICENSE
-'---------------------------------------------------->
-
 </body>
